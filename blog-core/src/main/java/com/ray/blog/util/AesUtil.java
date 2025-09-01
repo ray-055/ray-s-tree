95627836bb8ef5a5af58fe8f48609a30e3cd16dd
ray <jiejilu787835@163.com>
2025-09-02 04:36:34 +0800

    密码加密小bug修复

--- blog-core/src/main/java/com/ray/blog/util/AesUtil.java
+++ blog-core/src/main/java/com/ray/blog/util/AesUtil.java
@@ -9,35 +9,75 @@ import javax.crypto.spec.SecretKeySpec;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 
+/**
+ *
+ * @author yadong.zhang (yadong.zhang0415(a)gmail.com)
+ * @website https://docs.zhyd.me
+ * @version 1.0
+ * @date 2018/4/16 16:26
+ * @since 1.0
+ */
 public class AesUtil {
-    private static final String KEY = "AES";
-    private static final String ALGORITHM = "AES/ECB/PKCS5Padding";
+    private static final String KEY_ALGORITHM = "AES";
+    private static final String DEFAULT_CIPHER_ALGORITHM = "AES/ECB/PKCS5Padding";
 
-    public static String encrypt(String password, String content) throws Exception{
-        Cipher cipher = Cipher.getInstance(ALGORITHM);
+    /**
+     * AES加密
+     *
+     * @param passwd
+     *         加密的密钥
+     * @param content
+     *         需要加密的字符串
+     * @return 返回Base64转码后的加密数据
+     * @throws Exception
+     */
+    public static String encrypt(String passwd, String content) throws Exception {
+        // 创建密码器
+        Cipher cipher = Cipher.getInstance(DEFAULT_CIPHER_ALGORITHM);
 
-        byte[] bytecontent = content.getBytes("utf-8");
+        byte[] byteContent = content.getBytes("utf-8");
 
-        cipher.init(Cipher.ENCRYPT_MODE, getsecretKey(password));
+        // 初始化为加密模式的密码器
+        cipher.init(Cipher.ENCRYPT_MODE, getSecretKey(passwd));
 
-        byte[] result = cipher.doFinal(bytecontent);
+        // 加密
+        byte[] result = cipher.doFinal(byteContent);
 
+        //通过Base64转码返回
         return Base64.encodeBase64String(result);
     }
 
-    public static String decrypt(String password, String encrypted) throws Exception{
-        Cipher cipher = Cipher.getInstance(ALGORITHM);
+    /**
+     * AES解密
+     *
+     * @param passwd
+     *         加密的密钥
+     * @param encrypted
+     *         已加密的密文
+     * @return 返回解密后的数据
+     * @throws Exception
+     */
+    public static String decrypt(String passwd, String encrypted) throws Exception {
+        //实例化
+        Cipher cipher = Cipher.getInstance(DEFAULT_CIPHER_ALGORITHM);
 
-        cipher.init(Cipher.DECRYPT_MODE, getsecretKey(password));
+        //使用密钥初始化，设置为解密模式
+        cipher.init(Cipher.DECRYPT_MODE, getSecretKey(passwd));
 
+        //执行操作
         byte[] result = cipher.doFinal(Base64.decodeBase64(encrypted));
 
         return new String(result, "utf-8");
     }
 
-    private static SecretKeySpec getsecretKey(final String password) throws NoSuchAlgorithmException {
+    /**
+     * 生成加密秘钥
+     *
+     * @return
+     */
+    private static SecretKeySpec getSecretKey(final String password) throws NoSuchAlgorithmException {
         //返回生成指定算法密钥生成器的 KeyGenerator 对象
-        KeyGenerator kg = KeyGenerator.getInstance(ALGORITHM);
+        KeyGenerator kg = KeyGenerator.getInstance(KEY_ALGORITHM);
         // javax.crypto.BadPaddingException: Given final block not properly padded解决方案
         // https://www.cnblogs.com/zempty/p/4318902.html - 用此法解决的
         // https://www.cnblogs.com/digdeep/p/5580244.html - 留作参考吧
@@ -49,6 +89,7 @@ public class AesUtil {
         //生成一个密钥
         SecretKey secretKey = kg.generateKey();
         // 转换为AES专用密钥
-        return new SecretKeySpec(secretKey.getEncoded(), ALGORITHM);
+        return new SecretKeySpec(secretKey.getEncoded(), KEY_ALGORITHM);
     }
+
 }
